b93c566f6517f82a6a13abdda1d60a61a49f65fd
Xabier Casan <xabier_abia@hotmail.com>
2019-03-26 22:37:36 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,7 @@
 require "pry"
+
 def alphabetize(arr)
   esperanto_alphabet=" abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by {|arr| arr.split('').collect{|char|esperanto_alphabet.index(char)}}
+  binding.pry
 end
